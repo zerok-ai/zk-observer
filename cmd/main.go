cd82843a35fb591100c1003bba74ada2ee94e47d
CHNG7 <nikhilchinta7@gmail.com>
2024-02-06 11:56:55 +0530

    moved http server code from main to httpserver package

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/golang/protobuf/proto"
 	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/kataras/iris/v12"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/zerok-ai/zk-observer/config"
 	"github.com/zerok-ai/zk-observer/handler"
 	promMetrics "github.com/zerok-ai/zk-observer/metrics"
@@ -19,14 +16,11 @@ import (
 	pb "go.opentelemetry.io/proto/otlp/collector/trace/v1"
 	"google.golang.org/grpc"
 	"net"
-	"net/http"
 	"os"
-	"time"
 )
 
 var mainLogTag = "main"
 var ctx = context.Background()
-var podIp = os.Getenv("POD_IP")
 
 type Args struct {
 	ConfigPath string
@@ -72,98 +66,17 @@ func main() {
 	logger.Debug(mainLogTag, "Started grpc server.")
 
 	//Creating http/protobuf server
-
-	app := newApp()
-	irisConfig := iris.WithConfiguration(iris.Configuration{
-		DisablePathCorrection: true,
-		LogLevel:              otlpConfig.Logs.Level,
-	})
-
-	app.Get("/metrics", iris.FromStd(promhttp.Handler()))
-
-	// Define a route to expose expvar data
-	app.Get("/debug/vars", iris.FromStd(http.DefaultServeMux))
-	app.Get("/healthz", func(ctx iris.Context) {
-		ctx.StatusCode(iris.StatusOK)
-	})
-
-	app.Post("/v1/traces", traceHandler.ServeHTTP)
-	configureBadgerGetStreamAPI(app, traceHandler)
-
-	// Start the server with timeouts
-	srv := &http.Server{
-		Addr:         ":" + otlpConfig.Port,
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-	err = app.Run(iris.Server(srv), irisConfig)
-
+	// Instantiate the HTTPServer
+	httpServer := server.NewHTTPServer()
+	// Configure routes and pass the traceHandler
+	httpServer.ConfigureRoutes(traceHandler)
+	// Run the HTTP server with the specified port and configs
+	err = httpServer.Run(*otlpConfig)
 	if err != nil {
 		logger.Error(mainLogTag, "Error starting the server:", err)
 	}
+	logger.Debug(mainLogTag, "Started http server.")
 
-	//badger
-	//newBadgerApp := newBadgerApp()
-
-	//err = newBadgerApp.Run(iris.Addr(":"+"8047"), irisConfig)
-	//if err != nil {
-	//	logger.Error(mainLogTag, "Error starting the server:", err)
-	//}
-
-}
-
-func newApp() *iris.Application {
-	app := iris.Default()
-
-	crs := func(ctx iris.Context) {
-		ctx.Header("Access-Control-Allow-Credentials", "true")
-
-		if ctx.Method() == iris.MethodOptions {
-			ctx.Header("Access-Control-Methods", "POST")
-
-			ctx.Header("Access-Control-Allow-Headers",
-				"Access-Control-Allow-Origin,Content-Type")
-
-			ctx.Header("Access-Control-Max-Age",
-				"86400")
-
-			ctx.StatusCode(iris.StatusNoContent)
-			return
-		}
-
-		ctx.Next()
-	}
-	app.UseRouter(crs)
-	app.AllowMethods(iris.MethodOptions)
-
-	return app
-}
-
-func newBadgerApp() *iris.Application {
-	app := iris.Default()
-
-	crs := func(ctx iris.Context) {
-		ctx.Header("Access-Control-Allow-Credentials", "true")
-
-		if ctx.Method() == iris.MethodOptions {
-			ctx.Header("Access-Control-Methods", "POST")
-
-			ctx.Header("Access-Control-Allow-Headers",
-				"Access-Control-Allow-Origin,Content-Type")
-
-			ctx.Header("Access-Control-Max-Age",
-				"86400")
-
-			ctx.StatusCode(iris.StatusNoContent)
-			return
-		}
-
-		ctx.Next()
-	}
-	app.UseRouter(crs)
-	app.AllowMethods(iris.MethodOptions)
-
-	return app
 }
 
 func ProcessArgs(cfg interface{}) Args {
@@ -183,58 +96,4 @@ func ProcessArgs(cfg interface{}) Args {
 
 	f.Parse(os.Args[1:])
 	return a
-}
-
-func configureBadgerGetStreamAPI(app *iris.Application, traceHandler *handler.TraceHandler) {
-	app.Post("get-trace-data", func(ctx iris.Context) {
-
-		logger.Fatal(mainLogTag, "Request Received to get span data from SM")
-
-		var inputList []string
-		promMetrics.TotalFetchRequestsFromSM.WithLabelValues(podIp).Inc()
-		// Read the JSON input containing the list of strings
-		if err := ctx.ReadJSON(&inputList); err != nil {
-			ctx.StatusCode(iris.StatusBadRequest)
-			err := ctx.JSON(iris.Map{"error": "Invalid JSON input"})
-			logger.Info(mainLogTag, fmt.Sprintf("Request Received to get span data : %s", inputList))
-			if err != nil {
-				promMetrics.TotalFetchRequestsFromSMError.WithLabelValues(podIp).Inc()
-				logger.Error(mainLogTag, "Invalid request format for fetching badger data for trace prefix list ", err)
-				return
-			}
-			return
-		}
-
-		//total traces span data requested from receiver
-		promMetrics.TotalTracesSpanDataRequestedFromReceiver.WithLabelValues(podIp).Add(float64(len(inputList)))
-
-		data, err2 := traceHandler.GetBulkDataFromBadgerForPrefix(inputList)
-		if err2 != nil {
-			promMetrics.TotalFetchRequestsFromSMError.WithLabelValues(podIp).Inc()
-			logger.Error(mainLogTag, fmt.Sprintf("Unable to fetch data from badger for tracePrefixList: %s", inputList), err2)
-			ctx.StatusCode(iris.StatusInternalServerError)
-			return
-		}
-		ctx.StatusCode(iris.StatusOK)
-		//jsonData, err := json.Marshal(data)
-		//if err != nil {
-		//	logger.Error(mainLogTag, fmt.Sprintf("Unable to fetch data from badger for tracePrefixList: %s", inputList), err)
-		//	return
-		//}
-
-		protoData, err := proto.Marshal(data)
-		if err != nil {
-			promMetrics.TotalFetchRequestsFromSMError.WithLabelValues(podIp).Inc()
-			logger.Error(mainLogTag, fmt.Sprintf("Unable to fetch data from badger for tracePrefixList: %s", inputList), err)
-			return
-		}
-		ctx.ContentType("application/octet-stream")
-		_, err = ctx.Write(protoData)
-		if err != nil {
-			logger.Error(mainLogTag, fmt.Sprintf("Unable to fetch data from badger for trace prefix list: %s", inputList), err)
-			return
-		}
-		promMetrics.TotalFetchRequestsFromSMSuccess.WithLabelValues(podIp).Inc()
-
-	}).Describe("Badger Data Fetch API")
 }
